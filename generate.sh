a7ec8b1b7cee56038b1cb540f761e38059b35096
pavel savinko <n.b.d@list.ru>
2023-05-22 18:50:51 +0300

    1

---
+++ generate.sh
@@ -0,0 +1,5 @@
+flutter pub run pigeon \
+  --input pigeons/messages.dart \
+  --dart_out lib/pigeon.dart \
+  --kotlin_out android/src/main/kotlin/com/savinkopav/flutter_geolocation_provider/Pigeon.kt \
+  --kotlin_package "com.savinkopav.flutter_geolocation_provider"
