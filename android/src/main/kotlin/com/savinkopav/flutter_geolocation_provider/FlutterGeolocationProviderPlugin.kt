e41adf3da068ce6c1df41df552acdf8269538df7
pavel savinko <n.b.d@list.ru>
2023-05-22 19:09:05 +0300

    6

--- android/src/main/kotlin/com/savinkopav/flutter_geolocation_provider/FlutterGeolocationProviderPlugin.kt
+++ android/src/main/kotlin/com/savinkopav/flutter_geolocation_provider/FlutterGeolocationProviderPlugin.kt
@@ -1,24 +1,26 @@
 package com.savinkopav.flutter_geolocation_provider
 
-import androidx.annotation.NonNull
-
+import android.app.Activity
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** FlutterGeolocationProviderPlugin */
-class FlutterGeolocationProviderPlugin: FlutterPlugin, MethodCallHandler {
+class FlutterGeolocationProviderPlugin: FlutterPlugin, ActivityAware, MethodCallHandler {
 
   private lateinit var channel : MethodChannel
+  private var activity: Activity? = null
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_geolocation_provider")
     channel.setMethodCallHandler(this)
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+  override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else {
@@ -26,7 +28,27 @@ class FlutterGeolocationProviderPlugin: FlutterPlugin, MethodCallHandler {
     }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    binding.activity.let {
+      activity = it
+    }
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    activity = null
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    binding.activity.let {
+      activity = it
+    }
+  }
+
+  override fun onDetachedFromActivity() {
+    activity = null
   }
 }
