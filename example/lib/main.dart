7ac3034e6bfa04cd05031268f00ee16cd8ff07c4
pavel savinko <n.b.d@list.ru>
2023-05-22 19:06:13 +0300

    5

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter/services.dart';
-import 'package:flutter_geolocation_provider/flutter_geolocation_provider.dart';
 import 'package:flutter_geolocation_provider/pigeon.dart';
 
 void main() {
@@ -16,8 +15,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Location _platformVersion = Location();
-  final _flutterGeolocationProviderPlugin = SimpleGeolocationApi();
+  String _latitude = 'Unknown';
+  String _longitude = 'Unknown';
+  final _simpleGeolocationProviderPlugin = SimpleGeolocationApi();
 
   @override
   void initState() {
@@ -26,31 +26,37 @@ class _MyAppState extends State<MyApp> {
   }
 
   Future<void> init() async {
+    Location location;
     try {
-      _platformVersion = await _flutterGeolocationProviderPlugin.getLastLocation();
+      location = await _simpleGeolocationProviderPlugin.getLastLocation();
     } on PlatformException {
-      _platformVersion = Location();
+      location = Location();
     }
 
     if (!mounted) return;
 
     setState(() {
-
+      _latitude = location.latitude?.toString() ?? "1";
+      _longitude = location.longitude?.toString() ?? "2";
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Column(
-          children: [
-            Text('latitude: ${_platformVersion.latitude}\n'),
-            Text('longitude: ${_platformVersion.longitude}\n'),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('latitude: $_latitude'),
+              Text('longitude: $_longitude')
+            ],
+          ),
         ),
       ),
     );
